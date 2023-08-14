8a8d49c2a96d098c77bb02459b18500169967b34
onejajae <onejajae@gmail.com>
2023-08-15 05:23:54 +0900

    Fix: change links with icon to bootstrap icon-link class

--- frontend/src/pages/NotFound.svelte
+++ frontend/src/pages/NotFound.svelte
@@ -2,10 +2,10 @@
     <div class="text-center my-5">
         <h1 class="display-1 fw-bold m-5">404</h1>
         <p class="fs-2">페이지를 찾을 수 없습니다.</p>
-        <a href="/" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
+        <a class="icon-link icon-link-hover" href="/">
             홈 화면으로 돌아가기
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
             </svg>
         </a>
     </div>
